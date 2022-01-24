1ac1125b4ddb038f467dad4743bb324d2b6e0591
Mohamed LOTFI <mohamed.lotfi@Digital-Grenoble.local>
2022-01-24 16:57:41 +0100

    ajout d'un header et du footer

---
+++ footer.php
@@ -0,0 +1,17 @@
+<div class="footer">
+        <ul>
+          <li><a href="../index.html">Accueil</a></li>
+          <li><a href="../contact/project V/contact.html">Contact</a></li>
+          <li><a href=""><img src="../img/instagram-svgrepo-com.svg" alt="instagram" class="icone"></a>
+            <a href=""><img src="../img/facebook-svgrepo-com.svg" alt="facebook" class="icone"></a>
+            <a href=""><img src="../img/youtube-svgrepo-com.svg" alt="youtube" class="icone"></a>
+          </li>
+        </ul>
+      </div>
+
+
+
+
+    
+</body>
+</html>
---
+++ item.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta names="desciption" content="Catalogues des diférents continents et pays où se trouvent des activités.">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/produit.css">
+</head>
+<body>
+<?php    
+include 'header.php';
+?>
+<?php
+$nom = "oeuf de fabergé";
+$prix = "1000000 euro";
+$image = "https://brocantemeg-antique.com/attachments/Image/232482.png?template=generic";
+?>
+<?php
+include 'footer.php';
+?>
+
+</body>
+</html>
+
