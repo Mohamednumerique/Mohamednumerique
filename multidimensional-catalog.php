e5de0cf61026c82108bee634ade91aaec4a378a8
Mohamed LOTFI <mohamed.lotfi@Digital-Grenoble.local>
2022-01-28 13:07:09 +0100

    ajout de la liste des attributs des porduits

---
+++ multidimensional-catalog.php
@@ -0,0 +1,30 @@
+
+<?php
+$products = [
+    "oeuf_Pamyat_Azova" => [
+    "name" => "oeuf_Pamyat_Azova",
+    "price" => 1000000,
+    "weight" => 125,
+    "discount" => null,
+    "picture_url" => "https://brocantemeg-antique.com/attachments/Image/232482.png?template=generic"
+    ],
+    "oeuf_du_Caucase" => [
+    "name" => "oeuf_du_Caucase",
+    "price" => 1500000,
+    "weight" => 105,
+    "discount" => null,
+    "picture_url" => "https://cdn-apple.com/ipad.jpg"],
+    ];
+    
+    
+
+
+    foreach($products as $produit){
+        foreach($produit as $carat => $valeur){
+            echo $carat. " : " . $valeur."<br>";
+        }
+        echo "<br>";
+    }
+
+
+    ?>
