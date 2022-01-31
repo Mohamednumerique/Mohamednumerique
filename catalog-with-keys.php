703646b6322315cef4aa44c98de5fa93cc17c358
Mohamed LOTFI <mohamed.lotfi@Digital-Grenoble.local>
2022-01-31 16:47:06 +0100

    ajout du fichier catalog-with-keys.php

--- catalog-with-keys.php
+++ catalog-with-keys.php
@@ -4,14 +4,15 @@ $oeuf_Pamyat_Azova  = [
 "name" => "Œuf de Fabergé (l’œuf du Caucase)";
 "price" => 1000000;
 "weight" => 125;
-"discount" => 0;
+"discount" => null;
 "picture_url" => "https://brocantemeg-antique.com/attachments/Image/232482.png?template=generic"];
+
 
 $oeuf_du_Caucase = [
     "name" => "Œuf de Fabergé (Œuf au Pamyat Azova)";
     "price" => 1500000;
     "weight" => 105;
-    "discount" => 0;
+    "discount" => null;
     "picture_url" => "https://iletaitunefoislebijou.fr/wp-content/uploads/2020/04/9-oeuf-du-caucase_faberge_il-%C3%A9tait-une-fois-le-bijou.jpg"];
     
     print_r($oeuf_du_Caucase);
